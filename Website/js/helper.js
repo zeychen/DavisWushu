ae80776bb93a6565e28661fb493cdde5530b17af
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-07-17 14:20:12 -0700

    - organized files into respective folders - created scss files with all colors declared - html text complete for main page

---
+++ Website/js/helper.js
@@ -0,0 +1,6 @@
+// Webpage text helper
+var HTMLaboutText = '<p class="description-text"></p>';
+var HTMLpracticeText = '<p class="description-text"></p>';
+var HTMLmembersText = '<p class="description-text"></p>';
+var HTMLofficersText = '<p class="description-text"></p>';
+var HTMLcoachesText = '<p class="description-text"></p>';

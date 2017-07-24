a6ce1ae05c0b0b23763b1a0a596ff14ceec5e1dd
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-07-24 12:15:04 -0700

    header animation complete - hide header welcome - move video off screen - change navbar background color to primary background color

--- Website/js/helper.js
+++ Website/js/helper.js
@@ -4,3 +4,34 @@ var HTMLpracticeText = '<p class="description-text"></p>';
 var HTMLmembersText = '<p class="description-text"></p>';
 var HTMLofficersText = '<p class="description-text"></p>';
 var HTMLcoachesText = '<p class="description-text"></p>';
+
+
+
+// on scroll, horizontal center nav bar and have it fixed on screen with primary bkgd color
+
+
+// translate video so only bottom 100px is showing
+// transition video to solid
+
+$(window).scroll(function(){
+	if($(this).scrollTop() > 0){
+		$('.tint').css('position','fixed');
+		$('.tint').animate({
+			backgroundColor: '#032e62'
+		}, 2500);
+		$('.video-foreground, .tint').addClass('anim');
+		$('.welcome').hide(100);
+
+		$('.header').animate({
+			'height': '12vh',
+		}, {
+	    	'duration': 2000,
+	    	'specialEasing': {
+	      		height: 'easeOutQuad'
+	    	}
+		});
+	}
+	
+});
+
+

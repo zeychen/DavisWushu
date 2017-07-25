4a6992fa780d353e63cd16ac6764bc4132432f8c
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-07-24 21:00:20 -0700

    about section - members box added - separators added
    practice section
    - practice box added
    - separators added
    - practice info added

--- Website/js/helper.js
+++ Website/js/helper.js
@@ -6,32 +6,48 @@ var HTMLofficersText = '<p class="description-text"></p>';
 var HTMLcoachesText = '<p class="description-text"></p>';
 
 
+$('.upper-box').hide();
+$('.lower-box').hide();
+$('.about-text').hide();
+$('.about-bkgd h1').hide();
 
-// on scroll, horizontal center nav bar and have it fixed on screen with primary bkgd color
-
-
-// translate video so only bottom 100px is showing
-// transition video to solid
-
+// header animation
 $(window).scroll(function(){
 	if($(this).scrollTop() > 0){
-		$('.tint').css('position','fixed');
-		$('.tint').animate({
-			backgroundColor: '#032e62'
-		}, 2500);
 		$('.video-foreground, .tint').addClass('anim');
-		$('.welcome').hide(100);
+		$('.welcome').hide();
 
 		$('.header').animate({
-			'height': '12vh',
+			'height': '10vh',
 		}, {
-	    	'duration': 2000,
+	    	'duration': 1000,
 	    	'specialEasing': {
 	      		height: 'easeOutQuad'
 	    	}
 		});
+		$('.tint').css('position','fixed');
+		$('.tint').animate({
+			backgroundColor: '#023b60'
+		}, 2500, 'linear', function(){
+			$('.upper-box').slideDown(1000, function(){
+				$('.about-text').show("slide", { 
+					direction: "left" 
+				}, 1000, function(){
+					$('.upper-box').css('clip-path', 'polygon(0% 0%, 100% 0%, 100% 50%, 0% 100%)');
+				});
+				$('#endurance').fadeIn(1000, function(){
+					$('#flexibility').fadeIn(1000, function(){
+						$('#strength').fadeIn(1000).delay(200);
+					}).delay(150);
+				}).delay(100);
+			});
+			$('.lower-box').show("slide", { 
+					direction: "right" 
+				}, 1000, function(){
+					$('.lower-box').css('clip-path', 'polygon(0% 50%, 100% 0%, 100% 100%, 0% 100%)');
+				});
+		});
 	}
-	
 });
 
 

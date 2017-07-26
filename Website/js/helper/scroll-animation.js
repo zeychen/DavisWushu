2aecfb0ec25a0e5014d4aebfd8c33f75614b424c
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-07-26 11:47:18 -0700

    parallax behavior added to about, members, and practice sections

---
+++ Website/js/helper/scroll-animation.js
@@ -0,0 +1,143 @@
+
+$(document).ready(function(){
+	var ofHeight = $(window).height()*(1/4);
+	// var tfHeight = $(window).height()*(3/4);
+
+	/* =============== Page Controller =============== */
+	var leaveController = new ScrollMagic.Controller({
+		globalSceneOptions: {
+			triggerHook: 'onLeave'
+		}
+	});
+
+	var centerController = new ScrollMagic.Controller({
+		globalSceneOptions: {
+			triggerHook: 'onCenter'
+		}
+	});
+
+	var enterController = new ScrollMagic.Controller({
+		globalSceneOptions: {
+			triggerHook: "onEnter"
+		}
+	});
+
+	/* =============== Parallax =============== */
+	var parallax1 = new ScrollMagic.Scene({triggerElement: "#about-section"})
+		.setTween("#about-bkgd-box, #about-lower-box", {y: "80%", ease: Linear.easeNone})
+		.addIndicators({name: "parallax 1"})
+		.addTo(centerController);
+
+	var parallax2 = new ScrollMagic.Scene({triggerElement: "#practice-section"})
+		.setTween("#practice-box", 2, {y: "90%", ease: Power2.easeInOut, opacity: 1})
+		.addIndicators({name: "parallax 2"})
+		.addTo(centerController);
+
+	var parallax3 = new ScrollMagic.Scene({triggerElement: "#practice-section"})
+		.setTween("#practice-info", {y: "60%", ease: Linear.easeNone, opacity: 1})
+		.addIndicators({name: "parallax 3"})
+		.addTo(centerController);
+		
+	var parallax4 = new ScrollMagic.Scene({triggerElement: "#practice-section"})
+		.setTween("#practice-lower-box", {y: "80%", ease: Linear.easeNone})
+		.addIndicators({name: "parallax 4"})
+		.addTo(centerController);
+
+	var parallax5 = new ScrollMagic.Scene({triggerElement: "#trigger"})
+		.setTween("#practice-section", {y: "50%", ease: Linear.easeNone})
+		.addIndicators({name: "parallax 5"})
+		.addTo(centerController);
+
+	/* =============== Animation =============== */
+	// var fiveStance = TweenMax.to();
+	// var parallax6 = new ScrollMagic.Scene({triggerElement: "#practice-info"})
+	// 	.setTween(fiveStance)
+	// 	.addIndicators({name: "5 stance"})
+	// 	.addTo(centerController);
+
+	/* =============== Nav Bar =============== */
+	var navBar = new ScrollMagic.Scene({offset: 100})
+		.setPin('#nav')
+		.setTween('#nav', 0.1, {backgroundColor: '#032e62'})
+		.addIndicators({name: "1"})
+		.addTo(leaveController);
+
+	/* =============== Welcome Video =============== */
+	var blurElement = {a:0};
+	var welcomeVid = new ScrollMagic.Scene({offset: 50})
+		.setTween(blurElement, 0.5, {a:5, onUpdate:applyBlur})
+		.addIndicators({name: "3"})
+		.addTo(leaveController);
+
+	/* =============== Welcome Text =============== */
+	var welcomeText = new ScrollMagic.Scene({offset: 50})
+		.setTween('#welcome', 1, {opacity: 0})
+		.addIndicators({name: "2"})
+		.addTo(leaveController);
+
+	/* =============== About Section =============== */
+	var aboutSection = new ScrollMagic.Scene({offset: ofHeight})
+		.setTween('#about-text', 1, {marginLeft: '10vw'})
+		.addIndicators({name: "4"})
+		.addTo(leaveController);
+
+	var aboutBkgdTween = new TweenMax.to('#endurance', 1, {opacity: '0.5'});
+
+	var endurance = new ScrollMagic.Scene({triggerElement: '#about-section'})
+		.setTween(aboutBkgdTween)
+		.addIndicators({name: "6"})
+		.addTo(centerController);
+
+	/* =============== Practice Section =============== */
+	var practiceSection = new ScrollMagic.Scene({triggerElement: '#practice-section'})
+		.setTween('#practice-title', 2, {marginLeft: '10vw'})
+		.addIndicators({name: "5"})
+		.addTo(centerController);
+
+	var socialLink = new ScrollMagic.Scene({triggerElement: '#trigger2'})
+		.setTween('.social-link', 1, {opacity: 1, y: '20px'})
+		.addIndicators({name: "social text"})
+		.addTo(centerController);
+
+	var socialText1 = new ScrollMagic.Scene({triggerElement: '#trigger2'})
+		.setTween('#social1', 0.5, {y: '-20px'})
+		.addIndicators({name: "social text"})
+		.addTo(centerController);
+
+	var socialText2 = new ScrollMagic.Scene({triggerElement: '#trigger2'})
+		.setTween('#social2', 0.5, {y: '20px'})
+		.addIndicators({name: "social text"})
+		.addTo(centerController);
+
+	var socialText2 = new ScrollMagic.Scene({triggerElement: '#trigger2'})
+		.setTween('#social3', 0.5, {y: '50px'})
+		.addIndicators({name: "social text"})
+		.addTo(centerController);
+
+	/* =============== Helper Functions =============== */
+	// change controller to scroll instead of jump
+	leaveController.scrollTo(function (newpos) {
+		TweenMax.to(window, 3, {scrollTo: {y: newpos}, ease: Power2.easeOut});
+	});
+
+	//  bind scroll to anchor links
+	$(document).on('click', "a[href^='#']", function (e) {
+		var id = $(this).attr("href");
+		if ($(id).length > 0) {
+			e.preventDefault();
+
+			// trigger scroll
+			leaveController.scrollTo(id);
+
+				// if supported by the browser we can even update the URL.
+			if (window.history && window.history.pushState) {
+				history.pushState("", document.title, id);
+			}
+		}
+	});
+
+	// blur function
+	function applyBlur(){
+		TweenMax.set(['#video-foreground'], {webkitFilter:"blur(" + blurElement.a + "px)",filter:"blur(" + blurElement.a + "px)"});
+	}
+});

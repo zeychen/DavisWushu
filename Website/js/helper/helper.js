5afc033008f546cf455954e8a79cdee64487cf87
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-07-28 18:47:56 -0700

    home text refactoring done

--- Website/js/helper/helper.js
+++ Website/js/helper/helper.js
@@ -1,22 +1,36 @@
 // Webpage text helper
 /* ============================= Global ============================= */
-// header
-var HTMLnav1Text = '<p class="description-text"></p>';
+// headers and texts
+var HTMLaboutHeader = '<p class="description-text"></p>';
+var HTMLpracticeHeader = '<p class="description-text"></p>';
+var HTMLgalleryHeader = '<p class="description-text"></p>';
+var HTMLofficersHeader = '<p class="description-text"></p>';
+var HTMLcoachesHeader = '<p class="description-text"></p>';
+
+var HTMLaboutText = '<p class="description-text"></p>';
 var HTMLpracticeText = '<p class="description-text"></p>';
-var HTMLmembersText = '<p class="description-text"></p>';
+var HTMLgalleryText = '<p class="description-text"></p>';
 var HTMLofficersText = '<p class="description-text"></p>';
 var HTMLcoachesText = '<p class="description-text"></p>';
-
 
 
 /* ============================= Home Page ============================= */
-// constants
-var HTMLaboutText = '<p class="description-text"></p>';
-var HTMLpracticeText = '<p class="description-text"></p>';
-var HTMLmembersText = '<p class="description-text"></p>';
-var HTMLofficersText = '<p class="description-text"></p>';
-var HTMLcoachesText = '<p class="description-text"></p>';
+// welcome
+var HTMLheaderText1 = '<p class="headerText" id="headerText%num%">%text%</p>';
+var HTMLheaderText2 = '<p class="headerText" id="headerText%num%">%text%</p>';
+var HTMLheaderText3 = '<p class="headerText" id="headerText%num%">%text%</p>';
+var HTMLheaderText4 = '<p class="headerText" id="headerText%num%">%text%</p>';
+var HTMLheaderText5 = '<p class="headerText" id="headerText%num%">%text%</p>';
 
+// member box
+var HTMLmembersStart = '<div class="memberContainer"></div>';
+var HTMLmemberHeader = '<h1 class="memberHeader">%data%</h1>';
+var HTMLmemberText = '<p class="description-text" id="member-text">%data%</p>';
+var HTMLmemberLink = '<p class="member-link"><a href="%link%" class="memberLinkText">%text%</a></p>';
+
+var HTMLfbLink = '<p id="social3"><a href="%link%" target="_blank">%text%</a></p>';
+var HTMLinstagramLink = '<p id="social4"><a href="%link%" target="_blank">%text%</a></p>'
+var HTMLemailLink = '<a href="mailto:%link%">%text%</a>'
 
 /* ============================= Leaders Page ============================= */
 // constants
